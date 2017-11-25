45ba18c579ee254eb4a4ae95a932f8d9c877ff00
Mike Kenyon <mike.kenyon@gmail.com>
2017-11-25 10:27:16 -0500

    Automatically click the Do Not Challenge box

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,11 @@
+v1.2.0 &ndash; 2017-11-25
+
+Improvements
+
+- Checks the "Do not challenge me on this device" box.
+  I am assuming that if you have the extension installed,
+  you trust the machine.
+
 v1.1.0 &ndash; 2016-12-18
 
 Improvements
--- src/inject/okta-verify.js
+++ src/inject/okta-verify.js
@@ -7,10 +7,17 @@
     return document.querySelector('[data-se=sms-send-code]');
   }
 
+  function findDoNotChallengeMeCheckbox() {
+    return document.querySelector('[data-se-for-name="rememberDevice"]');
+  }
+
   function getButton() {
     var button = findPushNotifButton() || findSendSMSButton();
 
     if (button) {
+      var checkbox = findDoNotChallengeMeCheckbox();
+
+      checkbox.click();
       button.click();
     } else {
       setTimeout(getButton, 100);
